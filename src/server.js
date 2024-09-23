8f5c0b36f19f68cea46ea45589c9f35de4f1a8de
Cade Rupert <rupertcade@gmail.com>
2024-09-23 12:57:24 -0400

    testing

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const https = require("https");
+const express = require('express');
+const path = require('path');
+const session = require("express-session");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+const server = app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/html/index.html'));
+});
