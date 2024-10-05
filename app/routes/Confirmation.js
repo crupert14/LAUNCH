0a05e19007d6e3e7d1d7ee116f982db0ad7b241a
Cade Rupert <121654954+crupert14@users.noreply.github.com>
2024-10-05 14:33:05 -0400

    token

--- app/routes/Confirmation.js
+++ app/routes/Confirmation.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const jwt = require('jsonwebtoken');
 
 router.get('/:token', (req, res) => {
-    console.log(req.params.token);
+    try {
+        const decode = jwt.verify(token, process.env.WEBSITE_SECRET);
+        console.log(decode);
+    }
+    catch(err) {
+        res.render(path.join(__dirname, '../../app/views/Login.ejs'), {err: "Token invalid!"});
+    }
 })
 
 module.exports = router;
