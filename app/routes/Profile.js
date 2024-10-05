54577906255e58e122fad31d81f037bc7e9299f4
Cade Rupert <121654954+crupert14@users.noreply.github.com>
2024-10-05 14:59:09 -0400

    Access requirements

--- app/routes/Profile.js
+++ app/routes/Profile.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.render(path.join(__dirname, '../../app/views/Profile.ejs'), {profileName: ""});
+    if(req.session.user) {
+        res.render(path.join(__dirname, '../../app/views/Profile.ejs'), {profileName: ""});
+    }
+    else {
+        res.render(path.join(__dirname, '../../app/views/Login.ejs'), {err: "You need to login!"});
+    }
 })
 
 module.exports = router;
