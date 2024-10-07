5fa7e604ff2ce1edb118fe32a1e3726e7655d387
Cade Rupert <121654954+crupert14@users.noreply.github.com>
2024-10-07 13:54:06 -0400

    profile page

--- app/routes/ProfileInfo.js
+++ app/routes/ProfileInfo.js
@@ -3,40 +3,51 @@ const router = express.Router();
 const path = require('path');
 const { User } = require(path.join(__dirname, '../models/schemas.js'));
 
-router.put('/', async (req, res) => {
+router.post('/', async (req, res) => {
     const { username, firstname, lastname, email, addrLine1, addrLine2, addrLine3, city, state } = req.body;
 
     try {
-        const user = await User.findOne({ username: req.session.user.username });
+        const existingUsername = await User.findOne({
+            username: username
+        });
+        const existingEmail = await User.findOne({
+            email: email
+        });
 
-        // Check if the user exists
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
+        if(existingUsername || existingEmail) {
+            res.render(path.join(__dirname, '../../app/views/Login.ejs'), {err: "Username or email already in use"});
         }
 
-        // Update user details
-        user.username = username;
-        user.personal.firstname = firstname;
-        user.personal.lastname = lastname;
-        user.email = email;
-        user.address.address1 = addrLine1;
-        user.address.address2 = addrLine2;
-        user.address.address3 = addrLine3;
-        user.address.city = city;
-        user.address.state = state;
+        // Update the user details
+        const result = await User.updateOne(
+            { username: req.session.user.username }, // Find the user by session username
+            {
+                $set: {
+                    username: username,
+                    email: email,
+                    'personal.firstname': firstname,
+                    'personal.lastname': lastname,
+                    'address.address1': addrLine1,
+                    'address.address2': addrLine2,
+                    'address.address3': addrLine3,
+                    'address.city': city,
+                    'address.state': state
+                }
+            }
+        );
 
-        // Save the updated user document
-        await user.save();
+        if (result.matchedCount === 0) {
+            res.render(path.join(__dirname, '../../app/views/Login.ejs'), {err: "Internal error"});
+        }
 
-        res.status(200).json({ message: 'User updated successfully' });
     } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error', error: err.message });
+        res.render(path.join(__dirname, '../../app/views/Login.ejs'), {err: "Internal error"});
     }
 });
 
 router.get('/', (req, res) => {
-    res.render(path.join(__dirname, '../../app/views/Home.ejs'));
+    console.log(req.session.user);
+    res.render(path.join(__dirname, '../../app/views/Profile.ejs'));
 });
 
 module.exports = router;
